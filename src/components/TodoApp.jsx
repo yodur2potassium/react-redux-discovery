463c364d9e6c4cec2fda397b31d797a4f67941cd
baptiste <baptiste@troopers.email>
2017-02-08 15:01:29 +0100

    TodoHeader

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -8,6 +8,7 @@ export default class TodoApp extends React.Component {
     render() {
         return <div>
             <section className="todoapp">
+                <TodoHeader />
                 <TodoList todos={this.props.todos} filter={this.props.filter}/>
             </section>
         </div>
